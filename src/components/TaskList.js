d253e7599ac62c222201b8401dd39f3ffc3877f1
Alexander <al.x.falconer@gmail.com>
2022-07-08 14:00:00 +0100

    1st List commit

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,6 +1,17 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList(tasks) {
+
+  const displayTasks = tasks.map((task, index)=> (
+    <Task 
+      key={index} 
+      text={task.text}
+      category={task.category}
+      />
+  )
+)
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
