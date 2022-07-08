5f3ca0b9aa32ba7acbefd652c052fa65074bb2db
Alexander <al.x.falconer@gmail.com>
2022-07-08 14:00:00 +0100

    still wrong

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 function CategoryFilter() {
+  const buttons = categories.map((category) =>{
+ 
+      <button
+        key={category}
+        className={className}
+        onClick={() => onHandleCategory(category)}
+      >{category}</button>}
+    );
+  }
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
     </div>
   );
-}
 
 export default CategoryFilter;
